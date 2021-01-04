afd745ca496f38cbb4a261a360bceb73985efa10
ssplant <76212491+ssplant@users.noreply.github.com>
2021-01-04 16:57:26 +0900

    Add project files.

---
+++ LabWorkTwoWPF/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace LabWorkTwoWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            ShowText.Items.Add(WriteText.Text);
+            WriteText.Clear();
+        }
+    }
+}
